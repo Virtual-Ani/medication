b7c536b57c31253c58c53818ece0e17020af2a36
sum130 <sm3904@naver.com>
2024-05-07 18:23:21 +0900

    가끔 추적된다고 로그뜸_수정필요

--- Assets/cshFind.cs
+++ Assets/cshFind.cs
@@ -7,7 +7,7 @@ public class cshFind : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("start---daramiing!!");
     }
 
     // Update is called once per frame
