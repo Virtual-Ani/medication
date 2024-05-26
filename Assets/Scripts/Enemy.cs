e91e78becd36eae3d584eaa8d12673315aa03460
Ye Eun Lee <dev.yeun0308@gmail.com>
2024-05-26 17:46:36 +0900

    총 기능 수정
    1. Enemy가 총을 맞으면 destroy
    2. grab한 후 a,b,x,y중 한 버튼을 누르면 탄창 재장전

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    //생성과 파괴를 담당할 이벤트
+    //public UnityEvent OnCreated;
+    public UnityEvent OnDestroyed;
+
+    private float destroyDelay = 1f;
+    private bool isDestroyed = false;
+
+    /*void Start()
+    {
+        OnCreated?.Invoke();
+
+        MonsterManager.Instance.OnSpawned(this);
+    }*/
+
+    public void Destroy()
+    {
+        if (isDestroyed) return;
+
+        isDestroyed = true;
+
+        Destroy(gameObject, destroyDelay);
+
+        OnDestroyed?.Invoke();
+
+        //MonsterManager.Instance.OnDestroyed(this);
+    }
+
+}
--- Assets/Scripts/Weapon/Gun/Gun.cs
+++ Assets/Scripts/Weapon/Gun/Gun.cs
@@ -1,3 +1,4 @@
+using Meta.XR.BuildingBlocks;
 using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,17 +6,21 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 public class Gun : MonoBehaviour
 {
     public UnityEvent OnGrab;//잡을때
     public UnityEvent OnRelease;//놓을때
     private InputDevice targetDevice;
+
+    [SerializeField]public GameObject btnMapper;
     private void Start()
     {
         // 오른손 Oculus Touch 컨트롤러를 찾음
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         targetDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        btnMapper.SetActive(false);
     }
 
     private void Update()
@@ -35,21 +40,17 @@ public class Gun : MonoBehaviour
             // 여기에 그립 버튼을 눌렀을 때의 동작을 추가할 수 있습니다.
         }
     }
-    public void Grab(SelectEnterEventArgs args)
+    public void Grab()
     {
-        var interactor = args.interactorObject;
-        if(interactor is XRDirectInteractor)
-        {
-            OnGrab?.Invoke();
-        }
+        OnGrab?.Invoke();
+        Debug.Log("그랩");
+        btnMapper.SetActive(true);
     }
 
-    public void Release(SelectExitEventArgs args)
+    public void Release()
     {
-        var interactor = args.interactorObject;
-        if (interactor is XRDirectInteractor)
-        {
-            OnRelease?.Invoke();
-        }
+        OnRelease?.Invoke();
+        Debug.Log("놓기");
+        btnMapper.SetActive(false);
     }
 }
